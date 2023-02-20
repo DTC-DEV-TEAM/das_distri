48ae24d554c55ab0ff752599b61f34815ba19587
patrickDigits <patrickpunzalan@digits.ph>
2023-02-20 09:54:54 +0800

    add fillable

--- app/ReturnsHeaderDISTRI.php
+++ app/ReturnsHeaderDISTRI.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class ReturnsHeaderDISTRI extends Model
 {
     protected $table = 'returns_header_distribution';
+    protected $fillable = ['returns_status', 'returns_status_1'];
 }
