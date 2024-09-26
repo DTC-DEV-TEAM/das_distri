772da4910cbb6b631238a3d031db7cbb03cd2bb1
Piolo Arrabis <pioloarrabis@digits.ph>
2024-09-26 11:29:40 +0800

    fix distri query

--- app/Http/Controllers/AdminDistriReturnDiagnosingController.php
+++ app/Http/Controllers/AdminDistriReturnDiagnosingController.php
@@ -411,28 +411,7 @@ use PHPExcel_Style_Fill;
 			);
 
 	
-			if(CRUDBooster::myPrivilegeName() == "Service Center"){ 
-
-				$storeList = self::getStoreList();
-
-				$query->whereIn('returns_status_1',[ 
-					ReturnsStatus::REQUESTED, 
-					ReturnsStatus::TO_PRINT_SSR 
-				])
-				->where('transaction_type', 1)
-				->whereIn('returns_header_distribution.stores_id', $storeList);
-
-			} else if (CRUDBooster::myPrivilegeName() == "Tech Lead") {
-
-				$query->whereIn('returns_status_1', [
-					ReturnsStatus::TO_ASSIGN_INC, 
-					ReturnsStatus::TO_DIAGNOSE, 
-					ReturnsStatus::TO_TEST
-				])
-				->where('transaction_type', 0);
-				
-			}
-			else if (CRUDBooster::myPrivilegeName() == "RMA Technician") {
+			if (CRUDBooster::myPrivilegeName() == "RMA Technician") {
 	
 				$query->whereIn('returns_status_1', [
 					ReturnsStatus::TO_DIAGNOSE, 
